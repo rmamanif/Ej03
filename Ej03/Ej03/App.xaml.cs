2d2bbc0e3e77918b18f2bb208457f5df4e78bac6
Richard Mamani <richard.mamani@tecsup.edu.pe>
2021-09-14 23:05:35 -0500

    asd

---
+++ Ej03/Ej03/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+
+namespace Ej03
+{
+    
+    [Preserve(AllMembers =true)]
+    [ContentProperty(nameof(Source))]
+
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceprovider)
+        {
+            if (Source == null)
+                return null;
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ej03/Ej03/EntryCode.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ej03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryCode : ContentPage
+    {
+        public EntryCode()
+        {
+            InitializeComponent();
+
+            StackLayout stack = new StackLayout();
+            var entry = new Entry
+            {
+                Text = "This is an read-only entry"
+            };
+            stack.Children.Add(entry);
+            Content = stack;
+        }
+    }
+}
